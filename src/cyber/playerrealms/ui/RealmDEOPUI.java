ef60e05da06adb282e0ab6fefd16a0160b564bd0
Cyber <44707958+CyberL1@users.noreply.github.com>
2021-02-06 00:49:25 +0100

    Replace Main.getInstance().getConfig().getString() with Utils.getString()

--- src/cyber/playerrealms/ui/RealmDEOPUI.java
+++ src/cyber/playerrealms/ui/RealmDEOPUI.java
@@ -1,6 +1,5 @@
 package cyber.playerrealms.ui;
 
-import cyber.playerrealms.Main;
 import cyber.playerrealms.menu.Menu;
 import cyber.playerrealms.menu.PlayerMenuUtility;
 import cyber.playerrealms.utils.Utils;
@@ -32,15 +31,15 @@ public class RealmDEOPUI extends Menu {
         Player p = (Player) e.getWhoClicked();
         ItemStack item = e.getCurrentItem();
 
-        if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.create.name"))) {
+        if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.create.name"))) {
             p.closeInventory();
             Utils.createRealm(p);
-        } else if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.go.name"))) {
+        } else if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.go.name"))) {
             p.closeInventory();
             Utils.gotoRealm(p, p);
-        } else if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.gotolobby.name"))) {
+        } else if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.gotolobby.name"))) {
             Utils.gotoLobby(p);
-        } else if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.visit.name"))) {
+        } else if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.visit.name"))) {
             new RealmVisitUI(PlayerMenuUtility.getPlayerMenuUtility(p)).open();
         }
     }
@@ -52,11 +51,11 @@ public class RealmDEOPUI extends Menu {
         inventory.setItem(2, makeItem(Utils.getString("items.realms.gotolobby.item"), Utils.getString("items.realms.gotolobby.name")));
 
         if (!new File("realm-" + p.getName()).exists()) {
-            inventory.setItem(0, makeItem(Utils.getString("items.realms.create.item"), Main.getInstance().getConfig().getString("items.realms.create.name")));
+            inventory.setItem(0, makeItem(Utils.getString("items.realms.create.item"), Utils.getString("items.realms.create.name")));
         } else {
-            inventory.setItem(0, makeItem(Utils.getString("items.realms.go.item"), Main.getInstance().getConfig().getString("items.realms.go.name")));
+            inventory.setItem(0, makeItem(Utils.getString("items.realms.go.item"), Utils.getString("items.realms.go.name")));
         }
 
-        inventory.setItem(1, makeItem(Utils.getString("items.realms.visit.item"), Main.getInstance().getConfig().getString("items.realms.visit.name")));
+        inventory.setItem(1, makeItem(Utils.getString("items.realms.visit.item"), Utils.getString("items.realms.visit.name")));
     }
 }
--- src/cyber/playerrealms/ui/RealmOPUI.java
+++ src/cyber/playerrealms/ui/RealmOPUI.java
@@ -1,6 +1,5 @@
 package cyber.playerrealms.ui;
 
-import cyber.playerrealms.Main;
 import cyber.playerrealms.menu.Menu;
 import cyber.playerrealms.menu.PlayerMenuUtility;
 import cyber.playerrealms.utils.Utils;
@@ -32,15 +31,15 @@ public class RealmOPUI extends Menu {
         Player p = (Player) e.getWhoClicked();
         ItemStack item = e.getCurrentItem();
 
-        if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.create.item"))) {
+        if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.create.item"))) {
             p.closeInventory();
             Utils.createRealm(p);
-        } else if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.go.item"))) {
+        } else if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.go.item"))) {
             p.closeInventory();
             Utils.gotoRealm(p, p);
-        } else if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.gotolobby.item"))) {
+        } else if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.gotolobby.item"))) {
             Utils.gotoLobby(p);
-        } else if (item.getItemMeta().getDisplayName().equals(Main.getInstance().getConfig().getString("items.realms.visit.item"))) {
+        } else if (item.getItemMeta().getDisplayName().equals(Utils.getString("items.realms.visit.item"))) {
             new RealmVisitUI(PlayerMenuUtility.getPlayerMenuUtility(p)).open();
         }
     }
@@ -52,11 +51,11 @@ public class RealmOPUI extends Menu {
         inventory.setItem(2, makeItem(Utils.getString("items.realms.gotolobby.item"), Utils.getString("items.realms.gotolobby.name")));
 
         if (!new File("realm-" + p.getName()).exists()) {
-            inventory.setItem(0, makeItem(Utils.getString("items.realms.create.item"), Main.getInstance().getConfig().getString("items.realms.create.name")));
+            inventory.setItem(0, makeItem(Utils.getString("items.realms.create.item"), Utils.getString("items.realms.create.name")));
         } else {
-            inventory.setItem(0, makeItem(Utils.getString("items.realms.go.item"), Main.getInstance().getConfig().getString("items.realms.go.name")));
+            inventory.setItem(0, makeItem(Utils.getString("items.realms.go.item"), Utils.getString("items.realms.go.name")));
         }
 
-        inventory.setItem(1, makeItem(Utils.getString("items.realms.visit.item"), Main.getInstance().getConfig().getString("items.realms.visit.name")));
+        inventory.setItem(1, makeItem(Utils.getString("items.realms.visit.item"), Utils.getString("items.realms.visit.name")));
     }
 }
