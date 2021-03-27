5514649aa3a06edd120faf658cb84e38e6846626
Cyber <44707958+CyberL1@users.noreply.github.com>
2021-03-27 22:14:01 +0100

    Stop realms from autoload and add an update checker, check if realm is closed

--- src/cyber/playerrealms/ui/RealmManageUI.java
+++ src/cyber/playerrealms/ui/RealmManageUI.java
@@ -4,6 +4,7 @@ import cyber.playerrealms.menu.Menu;
 import cyber.playerrealms.menu.PlayerMenuUtility;
 import cyber.playerrealms.utils.RealmVisibility;
 import cyber.playerrealms.utils.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
@@ -46,6 +47,9 @@ public class RealmManageUI extends Menu {
         } else if (item.getItemMeta().getDisplayName().equals((Utils.getString("items.realms.manage.close.name")))) {
             p.closeInventory();
             Utils.closeRealm(p);
+        } else if (item.getItemMeta().getDisplayName().equals((Utils.getString("items.realms.manage.open.name")))) {
+            p.closeInventory();
+            Utils.openRealm(p);
         }
     }
 
@@ -55,7 +59,8 @@ public class RealmManageUI extends Menu {
 
         inventory.setItem(0, makeItem(Utils.getString("items.realms.manage.visibility.item"), Utils.getString("items.realms.manage.visibility.name"), Utils.getString("items.realms.manage.visibility.lore").replaceAll("%visibility%", String.valueOf(Utils.getRealmVisibility(Utils.getRealm(p))))));
         inventory.setItem(1, makeItem(Utils.getString("items.realms.manage.delete.item"), Utils.getString("items.realms.manage.delete.name"), Utils.getString("items.realms.manage.delete.lore")));
-        inventory.setItem(2, makeItem(Utils.getString("items.realms.manage.close.item"), Utils.getString("items.realms.manage.close.name")));
+        if (Bukkit.getWorld(Utils.getRealm(p)) != null) inventory.setItem(2, makeItem(Utils.getString("items.realms.manage.close.item"), Utils.getString("items.realms.manage.close.name")));
+        if (Bukkit.getWorld(Utils.getRealm(p)) == null) inventory.setItem(2, makeItem(Utils.getString("items.realms.manage.open.item"), Utils.getString("items.realms.manage.open.name")));
         inventory.setItem(8, makeItem(Utils.getString("items.close.item"), Utils.getString("items.close.name")));
 
     }
